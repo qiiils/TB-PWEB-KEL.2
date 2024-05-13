688758c1e20f5a3628961ca4bdfbd15f7903d57c
qiiils <001qils@gmail.com>
2024-05-13 14:11:33 +0700

    comment

--- routes/users.js
+++ routes/users.js
@@ -2,16 +2,7 @@ var express = require('express');
 var router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const passwordController = require('../controllers/authpassword');
-const authMiddleware = require('../middleware/auth');
 
-import {
-    getUsers,
-    createUsers,
-    updateUsers,
-    deleteUsers,
-    changePassword
-} from "../controllers/Users.js"
 
 router.get('/', (req, res) => {
   res.render('index');
@@ -21,18 +12,14 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
-router.get('/user/change-password', changePassword) => {
-  res.render('changepw');
-});
-
-router.post('/user/update-password', auth.authenticate, authpassword.updatePassword);
+// router.get('/user/change-password', changePassword);
 
 router.get('/user/dashboard', function(req, res, next) {
-  res.status(200).send('profile');
+  res.render('profile');
 });
 
 router.get('/user/profile', function(req, res, next) {
-  res.status(200).send('profile');
+  res.render('profile');
 });
 
 router.get('/user/edit-profile', function(req, res, next) {
